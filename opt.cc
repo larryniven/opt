a825722e69d15009cae1804b41944ba2111cb344
Hao Tang <haotang@ttic.edu>
2015-01-04 22:49:00 -0600

    Add update rule with constant step size and momentum

--- opt.cc
+++ opt.cc
@@ -2,6 +2,62 @@
 
 namespace opt {
 
+    void const_step_update(ebt::SparseVector& theta,
+        ebt::SparseVector const& grad,
+        double step_size)
+    {
+        for (auto& p: grad) {
+            theta(p.first) -= p.second * step_size;
+        }
+    }
+
+    void const_step_update(std::vector<double>& theta,
+        std::vector<double> const& grad,
+        double step_size)
+    {
+        for (int i = 0; i < theta.size(); ++i) {
+            theta.at(i) -= grad.at(i) * step_size;
+        }
+    }
+
+    void const_step_update_momentum(ebt::SparseVector& theta,
+        ebt::SparseVector& update,
+        ebt::SparseVector const& grad,
+        double momentum,
+        double step_size)
+    {
+        for (auto& p: update) {
+            p.second *= momentum;
+        }
+
+        for (auto& p: grad) {
+            update(p.first) += p.second * (1 - momentum);
+        }
+
+        for (auto& p: update) {
+            theta(p.first) -= p.second * step_size;
+        }
+    }
+
+    void const_step_update_momentum(std::vector<double>& theta,
+        std::vector<double>& update,
+        std::vector<double> const& grad,
+        double momentum,
+        double step_size)
+    {
+        for (int i = 0; i < update.size(); ++i) {
+            update.at(i) *= momentum;
+        }
+
+        for (int i = 0; i < update.size(); ++i) {
+            update.at(i) += grad.at(i) * (1 - momentum);
+        }
+
+        for (int i = 0; i < theta.size(); ++i) {
+            theta.at(i) -= update.at(i) * step_size;
+        }
+    }
+
     void pa_update(ebt::SparseVector& theta,
         ebt::SparseVector const& loss_grad,
         double loss)
@@ -19,6 +75,27 @@ namespace opt {
     
             for (auto& p: loss_grad) {
                 theta(p.first) -= p.second * step_size;
+            }
+        }
+    }
+    
+    void pa_update(std::vector<double>& theta,
+        std::vector<double> const& loss_grad,
+        double loss)
+    {
+        if (loss > 0) {
+            double grad_norm_sq = 0;
+    
+            for (auto& v: loss_grad) {
+                grad_norm_sq += v * v;
+            }
+    
+            double step_size = loss / grad_norm_sq;
+
+            std::cout << "[pa] step_size: " << step_size << std::endl;
+    
+            for (int i = 0; i < theta.size(); ++i) {
+                theta.at(i) -= loss_grad.at(i) * step_size;
             }
         }
     }
--- opt.h
+++ opt.h
@@ -5,8 +5,32 @@
 
 namespace opt {
 
+    void const_step_update(ebt::SparseVector& theta,
+        ebt::SparseVector const& grad,
+        double step_size);
+
+    void const_step_update(std::vector<double>& theta,
+        std::vector<double> const& grad,
+        double step_size);
+
+    void const_step_update_momentum(ebt::SparseVector& theta,
+        ebt::SparseVector& update,
+        ebt::SparseVector const& grad,
+        double momentum,
+        double step_size);
+
+    void const_step_update_momentum(std::vector<double>& theta,
+        std::vector<double>& update,
+        std::vector<double> const& grad,
+        double momentum,
+        double step_size);
+
     void pa_update(ebt::SparseVector& theta,
         ebt::SparseVector const& loss_grad,
+        double loss);
+    
+    void pa_update(std::vector<double>& theta,
+        std::vector<double> const& loss_grad,
         double loss);
     
     void adagrad_update(ebt::SparseVector& theta,
